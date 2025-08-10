24c4b45cf193bb46eb79219a9272bf3f8ebf385b
wighawag <wighawag@gmail.com>
2025-08-10 09:02:08 -0600

    add basic web setup

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Button from '$lib/ui/generic/Button.svelte';
+import { Provider, WalletUnlocked } from 'fuels';
+    function increment() {
+      console.log('Incrementing...');
+    }
+</script>
+
+<Button onclick={increment}>increment</Button>
