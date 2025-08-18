4c5b325d4aa584df62803ad35795669878e05a9d
wighawag <wighawag@gmail.com>
2025-08-18 09:53:34 -0600

    setup

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
-    import Button from '$lib/ui/generic/Button.svelte';
-import { Provider, WalletUnlocked } from 'fuels';
-    function increment() {
-      console.log('Incrementing...');
-    }
+	import Button from '$lib/ui/generic/Button.svelte';
+	import type { BN } from 'fuels';
+	import { Provider, Wallet, WalletUnlocked } from 'fuels';
+	import { PUBLIC_FUEL_NODE_URL } from '$env/static/public';
+
+	let error: string | undefined = $state(undefined);
+
+	async function increment() {
+		console.log('Incrementing...');
+		const provider = new Provider(PUBLIC_FUEL_NODE_URL);
+		const wallet: WalletUnlocked = Wallet.fromPrivateKey('0x', provider);
+
+		const balance: BN = await wallet.getBalance(await provider.getBaseAssetId());
+		console.log('Current balance:', balance.toString());
+	}
 </script>
 
 <Button onclick={increment}>increment</Button>
