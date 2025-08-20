d048b1026136e217df0cbbad03fcf3aba0967aaf
wighawag <wighawag@gmail.com>
2025-08-20 11:24:32 -0600

    wip: ts test

--- onchain/fuel/test/contract.test.ts
+++ onchain/fuel/test/contract.test.ts
@@ -3,9 +3,12 @@ import { launchTestNode, TestAssetId } from 'fuels/test-utils';
 import { describe, test, expect } from 'vitest';
 
 import { TestContractFactory } from '../typescript/src/contracts/TestContractFactory';
+import { ActionInput } from '../typescript/src/contracts/TestContract';
+import { Vec } from '../typescript/src/contracts/common';
+import { V } from 'vitest/dist/chunks/reporters.d.BFLkQcL6.js';
 
-describe('Calling contract methods', () => {
-  test('Call non-payable method', async () => {
+describe('Space', () => {
+  test('Commiting actions succeed', async () => {
     using testNode = await launchTestNode({
       contractsConfigs: [
         {
@@ -19,9 +22,32 @@ describe('Calling contract methods', () => {
     } = testNode;
 
     const { waitForResult: commitActionsCall } = await contract.functions.commit_actions("0x0000000000000000000000000000000000000000000000000000000000000001").call();
-    const { value: callInfo } = await commitActionsCall();
+    const { gasUsed } = await commitActionsCall();
 
-    console.log(callInfo);
+    console.log({gasUsed});
+  });
+
+
+  test('Commit and Reveal succeeds', async () => {
+     
+    let actions: Vec<ActionInput> = [];
+    actions.push({Activate: { system: 1 }});
+    actions.push({SendFleet: { from: 1,
+        spaceships: 100,
+        destination: {Known: 2}}});
+    
+      actions.push({SendFleet: { from: 1,
+      spaceships: 100,
+      destination: {Eventual: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"}}});
+    
+    let secret = 0x0000000000000000000000000000000000000000000000000000000000000001;
+    
+    // let hash = _hash_actions(actions, secret);
+    // caller.commit_actions(hash);
+
+    // caller.increase_time(COMMIT_PHASE_DURATION.as_seconds());
+
+    // caller.reveal_actions(identity, secret, actions);
   });
 
 });
